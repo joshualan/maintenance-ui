1b08d219afa5bc1eb72d1a0123bd1fd47ffc6f6c
Alan Estrada <joshualan23@yahoo.com>
2021-05-26 13:14:05 -0400

    Added resource grid and tenant/site selection

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "@progress/kendo-theme-material/dist/all.css";
 
-import DeviceGrid from './DeviceGrid';
+import DeviceGrid from "./DeviceGrid";
 
 // import { Grid, GridColumn } from "@progress/kendo-react-grid";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Barebones React App 2021"),
-  //   React.createElement("div", {}, "Hello, World!"),
-  // ]);
-
   return (
     <div>
-      <h1> Barebones React App 2021 </h1>
-      Hello, World!
+      <h1>WUG Resource List</h1>
       <DeviceGrid></DeviceGrid>
     </div>
   );
--- src/DeviceGrid.js
+++ src/DeviceGrid.js
@@ -1,12 +1,104 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Grid, GridColumn } from "@progress/kendo-react-grid";
+import "@progress/kendo-theme-material/dist/all.css";
 
 const DeviceGrid = () => {
-  const [tenants, setTenants] = useState([]);
-  const [sites, setSites] = useState([]);
-  const [devices, setDevices] = useState([]);
-  
+  const [tenant, setTenant] = useState("");
+  const [tenantsList, setTenantsList] = useState([]);
+  const [site, setSite] = useState("");
+  const [siteMap, setSiteMap] = useState({});
+  const [sitesList, setSitesList] = useState([]);
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    requestTenants();
+  }, []);
+
+  useEffect(() => {
+    setSitesList(siteMap[tenant] || []);
+  }, [tenant]);
+
+  async function requestTenants() {
+    const res = await fetch(
+      "https://tenantsandsites.azurewebsites.net/api/Sites?code=bmuHfXVJfQ8fDLmZFiN89e2/KdQc/rIpOT/6JctxQLLTfYcydER7SQ==",
+      { method: "GET", mode: "cors" }
+    );
+
+    const json = await res.json();
+    const m = {};
+
+    json.forEach((s) => {
+      m[s.TenantID] = m[s.TenantID] || [];
+      m[s.TenantID].push(s.SiteID);
+    });
+
+    setTenantsList(Object.keys(m));
+    setSiteMap(m);
+  }
+
+  async function requestResources() {
+    if (!tenant || !site) {
+      return;
+    }
+
+    const res = await fetch(
+      `https://wugdeviceconfighandler.azurewebsites.net/api/Resources/${tenant}/${site}?code=ohoA9yF29wlnpmZi3rmxapSi06KMfA60/QAF/oLls6/xoaL3k1sK2A==`,
+      { method: "GET", mode: "cors" }
+    );
+
+    const json = await res.json();
+    setResources(json);
+  }
+
   return (
-    <h1> This is the device grid.</h1>
+    <div className="search-params">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestResources();
+        }}
+      >
+        <label htmlFor="tenant">
+          Tenant
+          <select
+            id="tenant"
+            onChange={(e) => setTenant(e.target.value)}
+            onBlur={(e) => setTenant(e.target.value)}
+            value={tenant}
+          >
+            <option value=""></option>
+            {tenantsList.map((tenant) => (
+              <option value={tenant} key={tenant}>
+                {tenant}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="sites">
+          Site
+          <select
+            id="sites"
+            onChange={(e) => setSite(e.target.value)}
+            onBlur={(e) => setSite(e.target.value)}
+            value={site}
+          >
+            <option value=""></option>
+            {sitesList.map((site) => (
+              <option value={site} key={site}>
+                {site}
+              </option>
+            ))}
+          </select>
+          <button>Submit</button>
+        </label>
+      </form>
+
+      <Grid data={resources}>
+        <GridColumn field="DisplayName" title="Display Name" />
+        <GridColumn field="ResourceID" title="Resource ID" />
+        <GridColumn field="DefaultIPAddress" title="IP Address" />
+      </Grid>
+    </div>
   );
 };
 
