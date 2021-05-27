97322c8f50a2242a7c9898663063286e47ea37bd
Alan Estrada <joshualan23@yahoo.com>
2021-05-27 09:34:19 -0400

    Prettify

--- src/ResourceGrid.js
+++ src/ResourceGrid.js
@@ -5,32 +5,39 @@ import { process } from "@progress/kendo-data-query";
 import "@progress/kendo-theme-material/dist/all.css";
 
 const MaintenanceCell = (props) => {
-  const {dataItem} = props;
+  const { dataItem } = props;
   let inMaintenance = false;
-  if (dataItem.WorstStateInternalID === 2 || dataItem.BestStateInternalID === 2) {
-    inMaintenance = true; 
+  if (
+    dataItem.WorstStateInternalID === 2 ||
+    dataItem.BestStateInternalID === 2
+  ) {
+    inMaintenance = true;
   }
 
   return inMaintenance ? (
-    <td>In Maintenance <button>Toggle</button></td>
+    <td>
+      In Maintenance <button>Toggle</button>
+    </td>
   ) : (
-    <td>Not In Maintenance <button>Toggle</button></td>
-    
+    <td>
+      Not In Maintenance <button>Toggle</button>
+    </td>
   );
-}
+};
 
 const ResourceGrid = (props) => {
-  const {resources} = props;
+  const { resources } = props;
   const [dataState, setDataState] = useState({});
   const [result, setResult] = useState(resources);
 
   const onDataStateChange = (event) => {
     setDataState(event.dataState);
     setResult(process(resources, event.dataState));
-  }
+  };
 
   return (
-    <Grid data={result}
+    <Grid
+      data={result}
       filterable={true}
       onDataStateChange={onDataStateChange}
       {...dataState}
