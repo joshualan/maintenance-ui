7dd160a7fb61ecff1c4fa8d6acd3b823963f86be
Alan Estrada <joshualan23@yahoo.com>
2021-06-07 12:10:26 -0400

    Fixed the icon logic

--- src/utils/emoji.js
+++ src/utils/emoji.js
@@ -1,7 +1,7 @@
 const getEmojiFromMonitorStatus = (status) => {
-  if (status === 1) return "✔️";
+  if (status === 3) return "✔️";
   else if (status === 2) return "🛠️";
-  else if (status === 3) return "❌";
+  else if (status === 1) return "❌";
   else return "❓";
 };
 
